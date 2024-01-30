ee8ae51a37bfc9e0e5e3f06aa00e8e1a79e26f6c
rocoriyan <rcorrigan.other+github@gmail.com>
2024-01-30 16:56:40 +0000

    styling changes, got dates working

--- src/AmiiboModal.jsx
+++ src/AmiiboModal.jsx
@@ -9,22 +9,24 @@ const AmiiboModal = ({ isOpen, closeModal, amiibo }) => {
             <ReactModal isOpen={isOpen} onRequestClose={closeModal} className="modal">
                 <button className="modalCloser" onClick={closeModal}>CLOSE</button>
                 <h2>{amiibo.character}</h2>
+                <img src={amiibo.image} className="image"/>
                 <div className="infoContainer">
-                    <img src={amiibo.image} className="infoSect image"/>
-                    <div className="infoSect info">
+                    <div className="info">
                         <h3>amiibo Series</h3>
                         <p>{amiibo.amiiboSeries}</p>
                     </div>
-                    <div className="infoSect info">
+                    <div className="info">
                         <h3>Game Series</h3>
                         <p>{amiibo.gameSeries}</p>
                     </div>
-                    <div className="infoSect info list">
+                    <div className="info list">
                         <h3>Release</h3>
-                        {/*<p>AU: {amiibo.release.au}</p>
-                        <p>EU: {amiibo.release.eu}</p>
-                        <p>JP: {amiibo.release.jp}</p>
-                        <p>NA: {amiibo.release.na}</p>*/}
+                        <ul>
+                            <li>AU: {(amiibo.release).au}</li>
+                            <li>EU: {(amiibo.release).eu}</li>
+                            <li>JP: {(amiibo.release).jp}</li>
+                            <li>NA: {(amiibo.release).na}</li>
+                        </ul>
                     </div>
                 </div>
             </ReactModal>
